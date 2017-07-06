fa31fdca798cb21732ed98ceac26340a2ea741ba
ramdc <duran.ramon@uabc.edu.mx>
2017-07-05 22:55:05 -0700

    update message

---
+++ scripts/assignment_1/control.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import rospy
+from muro_summer_2017.msg import Control
+from muro_summer_2017.msg import Pose
+
+class controller():
+
+    def __init__(self):
+        #create the Robot node
+        rospy.init_node('control_sys',anonymous=True)
+        #create the publisher, topic name and object
+        self.control_pub = rospy.Publisher('ctrl_topic', Control, queue_size=10)
+        #create the subscriber
+        self.pose_sub = rospy.Subscriber('pose_topic', Pose, self.callback)
+        self.pose = Pose()
+        #define the loop frequency rate of 10 Hz
+        self.rate = rospy.Rate(10)
+#storage data every incoming message
+    def callback(self,data):
+        self.pose = data
+        self.pose.x = round(self.pose.x, 4)
+#compute controller value
+    def run_controller(self):
+        #create Pose instance and get the reference value
+        ref_pose = Pose()
+        ref_pose.x = input("set x reference value:")
+        #set the tolerance to stop the ctrl_val loop
+        tolerance = 0.0005
+        #set proportional gain value
+        kp = 0.2
+        #define ctrl_val instance
+        ctrl_val = Control()
+        ctrl_val.ref = ref_pose.x
+        #ctrl_val loop running @ 10Hz
+        while abs(ref_pose.x-self.pose.x) >= tolerance: #or (not rospy.is_shutdown()):
+            #Compute ctrl_val
+            ctrl_val.u = kp*(ref_pose.x-self.pose.x)
+            #publish ctrl_val
+            self.control_pub.publish(ctrl_val)
+            rospy.loginfo("Actual U_value: %f" % (ctrl_val.u))
+            self.rate.sleep()
+        #force to stop the robot
+        ctrl_val.u = 0
+        self.control_pub.publish(ctrl_val)
+        #keep the node running until stop.
+        rospy.spin()
+#main function
+if __name__ == '__main__':
+    try:
+        #create the u instance and run the controller
+        u = controller()
+        u.run_controller()
+    except rospy.ROSInterruptException: pass
---
+++ scripts/assignment_2/turtle_robot_controller.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from math import pow,atan2,sqrt,pi
+
+#This program moves a diferential WMR robot to a specified coordinate in xy frame
+#using a simple proportional controller for linear and angular velocity of the robot
+#The theory of this controller is detailed in [1] and a python example is shown
+# in http://wiki.ros.org/turtlesim/Tutorials/Go%20to%20Goal
+#[1] Corke, P. (2011). Robotics, vision and control: fundamental algorithms in MATLAB (Vol. 73). Springer.
+
+class robot_controller():
+
+    #inizialitation method
+    def __init__(self):
+        #create the node
+        rospy.init_node('RT_controller',anonymous = True)
+        #create the publiser and identyfy the topic
+        self.velocities_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
+        #create the subscriber and identify the topic
+        self.pose_sub = rospy.Subscriber('/turtle1/pose', Pose, self.callback)
+        #create the generalized q coordinate vector
+        self.q = Pose()
+        #define the loop freq rate @ 10Hz
+        self.rate = rospy.Rate(10)
+        #define limit values of the robot
+        self.steering_ang_limit = 3*pi/4
+        self.lin_velocity_limit = 3
+        self.d_tolerance = 0.005
+        #define gains
+        self.Kp_vel = 1.5
+        self.Kp_theta = 3
+
+    #update the q vector in every incomming message
+    def callback (self,data):
+        self.q = data
+        self.q.x = round(self.q.x, 4) #gets x position
+        self.q.y = round(self.q.y, 4) #gets y position
+        self.q.theta = round(self.q.theta, 5) #gets angular position
+
+    #computes the angular speed control signal
+    def ang_speed_controller(self, qref):
+        angular_vel = self.Kp_theta * (atan2(qref.y-self.q.y, qref.x-self.q.x)-self.q.theta)
+        if angular_vel > self.steering_ang_limit:
+            angular_vel = self.steering_ang_limit
+        elif angular_vel < (-self.steering_ang_limit):
+            angular_vel = -self.steering_ang_limit
+        return angular_vel
+
+    #computes the linear speed control signal
+    def lin_speed_controller(self, qref):
+        velocity = self.Kp_vel * (sqrt(pow((qref.x-self.q.x), 2)+pow((qref.y-self.q.y), 2)))
+        #set the saturation values for linear speed
+        if velocity > self.lin_velocity_limit:
+            velocity = self.lin_velocity_limit
+        elif velocity < (-self.lin_velocity_limit):
+            velocity = -self.lin_velocity_limit
+        #returns the computed velocity
+        return velocity
+
+    #moves the robot to a specified coordinate in xy frame
+    def move_to_coordinate(self):
+        qref = Pose()
+        #intoduce the reference coordinate
+        qref.x = input("Set x value:")
+        qref.y = input("Set y value:")
+        v_msg = Twist()
+        #sets to zero all unused magnitudes
+        v_msg.linear.y = 0
+        v_msg.linear.z = 0
+        v_msg.angular.x = 0
+        v_msg.angular.y = 0
+        #executes the controller while the distances from robot to set point is
+        #greather than than the specifies tolerance
+        while sqrt(pow((qref.x-self.q.x), 2)+pow((qref.y-self.q.y), 2))>self.d_tolerance:
+            #obtains linear and angular velocities from the their controllers
+            v_msg.linear.x = self.lin_speed_controller(qref)
+            v_msg.angular.z = self.ang_speed_controller(qref)
+            #publish the velocities
+            self.velocities_pub.publish(v_msg)
+            self.rate.sleep()
+        #stops the robot when the point is reached
+        v_msg.linear.x = 0
+        v_msg.angular.z = 0
+        self.velocities_pub.publish(v_msg)
+        #the robot has reached the set point
+        print "Done, press ctrl+c to close"
+        rospy.spin()
+
+#main function
+if __name__ == '__main__':
+    try:
+        #create the robot instance and execute the program
+        robot = robot_controller()
+        robot.move_to_coordinate()
+    except rospy.ROSInterruptException: pass
